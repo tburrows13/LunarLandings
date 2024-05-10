cdfbcd51eb924a0b9d9f2c8bb5c23a43f9d2cbf5
Tom Burrows <tomburrows13@icloud.com>
2024-05-10 17:47:23 +0100

    Picker dollies compat

---
+++ compatibility/picker-dollies/control.lua
@@ -0,0 +1,20 @@
+local function blacklist_entities()
+  if not script.active_mods["PickerDollies"] then return end
+  if not remote.interfaces["PickerDollies"] then return end
+  if not remote.interfaces["PickerDollies"]["add_blacklist_name"] then return end
+  -- Blacklist LL entities
+  remote.call("PickerDollies", "add_blacklist_name", "ll-arc-furnace")
+  remote.call("PickerDollies", "add_blacklist_name", "ll-arc-furnace-reactor")
+  remote.call("PickerDollies", "add_blacklist_name", "ll-arc-furnace-storage-tank")
+  remote.call("PickerDollies", "add_blacklist_name", "rocket-silo")
+  remote.call("PickerDollies", "add_blacklist_name", "rocket-silo-down")
+  remote.call("PickerDollies", "add_blacklist_name", "rocket-silo-interstellar")
+  remote.call("PickerDollies", "add_blacklist_name", "ll-landing-pad")
+  remote.call("PickerDollies", "add_blacklist_name", "steam-turbine")
+  remote.call("PickerDollies", "add_blacklist_name", "ll-steam-condenser")
+end
+
+return
+{
+  load = blacklist_entities
+}
--- scripts/compatibility.lua
+++ scripts/compatibility.lua
@@ -1,16 +1,16 @@
 --local better_victory = require "compatibility.better-victory-screen.control"
 --local disco_science  = require "compatibility.disco-science.control"
 local milestones     = require "compatibility.milestones.control"
---local picker_dollies = require "compatibility.picker-dollies.control"
+local picker_dollies = require "compatibility.picker-dollies.control"
 
 ---@type ScriptLib
 local Compatibility = {}
 
---[[Compatibility.on_load = function()
+Compatibility.on_load = function()
   picker_dollies.load()
 end
 
-Compatibility.on_init = function()
+--[[Compatibility.on_init = function()
   disco_science.init()
 end]]
 
