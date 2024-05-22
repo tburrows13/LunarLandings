da7397e382307d769a728a8ce36924425d3b6afb
Tom Burrows <tomburrows13@icloud.com>
2024-05-23 00:12:24 +0100

    Add "Earth-like Nauvis" map gen preset

---
+++ prototypes/map-gen-presets.lua
@@ -0,0 +1,17 @@
+local presets = data.raw["map-gen-presets"]["default"]
+
+presets["ll-earth-like"] = {
+  order = "a",
+  basic_settings = {
+    autoplace_controls = {
+      -- Nauvis climate
+      hot = {size = 0.5},
+      cold = {size = 0.5},
+    },
+    property_expression_names = {
+      -- More Nauvis climate
+      ["control-setting:moisture:bias"] = 0.05,
+      ["control-setting:aux:bias"] = -0.35
+    },
+  },
+}
