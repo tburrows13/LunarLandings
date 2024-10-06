3fc6290eda847425173febc83d43b310032bd517
Tom Burrows <tomburrows13@icloud.com>
2024-10-06 23:59:00 +0100

    Remove planet icons from autoplace localised_name

--- prototypes/autoplace-controls.lua
+++ prototypes/autoplace-controls.lua
@@ -1,15 +1,9 @@
-for _, autoplace_control in pairs(data.raw["autoplace-control"]) do
-  if autoplace_control.localised_name then
-    autoplace_control.localised_name = {"", "[virtual-signal=ll-nauvis] ", autoplace_control.localised_name}
-  end
-end
-
 data:extend(
 {
   {
     type = "autoplace-control",
     name = "ll-moon-rock",
-    localised_name = {"", "[virtual-signal=ll-luna] [entity=ll-moon-rock] ", {"entity-name.ll-moon-rock"}},
+    localised_name = {"", "[entity=ll-moon-rock] ", {"entity-name.ll-moon-rock"}},
     richness = true,
     order = "d-a",
     category = "resource"
@@ -17,7 +11,7 @@ data:extend(
   {
     type = "autoplace-control",
     name = "ll-ice",
-    localised_name = {"", "[virtual-signal=ll-luna] [entity=ll-ice] ", {"entity-name.ll-ice"}},
+    localised_name = {"", "[entity=ll-ice] ", {"entity-name.ll-ice"}},
     richness = true,
     order = "d-b",
     category = "resource"
@@ -25,7 +19,7 @@ data:extend(
   {
     type = "autoplace-control",
     name = "ll-rich-moon-rock",
-    localised_name = {"", "[virtual-signal=ll-luna] [entity=ll-rich-moon-rock] ", {"entity-name.ll-rich-moon-rock"}},
+    localised_name = {"", "[entity=ll-rich-moon-rock] ", {"entity-name.ll-rich-moon-rock"}},
     richness = true,
     order = "d-c",
     category = "resource"
@@ -33,7 +27,7 @@ data:extend(
   {
     type = "autoplace-control",
     name = "ll-astrocrystals",
-    localised_name = {"", "[virtual-signal=ll-luna] [entity=ll-astrocrystals] ", {"entity-name.ll-astrocrystals"}},
+    localised_name = {"", "[entity=ll-astrocrystals] ", {"entity-name.ll-astrocrystals"}},
     richness = true,
     order = "d-d",
     category = "resource",
