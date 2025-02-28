f86972174d97394bd52289c8ff1832f5726ce3f2
Tom Burrows <tomburrows13@icloud.com>
2025-02-28 19:53:06 +0000

    Fix migration

--- migrations/2025-02-28-cargo-pods.lua
+++ migrations/2025-02-28-cargo-pods.lua
@@ -1,3 +1,5 @@
+local Buckets = require "scripts.buckets"
+
 for _, player in pairs(game.players) do
   if player.opened then
     player.opened = nil
@@ -8,8 +10,10 @@ for _, landing_pad_data in pairs(storage.landing_pads) do
   landing_pad_data.cargo_pods_targeting = {}
 end
 
-for _, rocket_silo_data in pairs(storage.rocket_silos) do
-  if rocket_silo_data.auto_launch == "any" then
-    rocket_silo_data.auto_launch = "none"
+for _, rocket_silo_data_bucket in pairs(storage.rocket_silos.list) do
+  for _, rocket_silo_data in pairs(rocket_silo_data_bucket) do
+    if rocket_silo_data.auto_launch == "any" then
+      rocket_silo_data.auto_launch = "none"
+    end
   end
 end
