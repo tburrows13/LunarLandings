1de9e0877d2b5b05bf2c3e941d3426e95e181cd6
Tom Burrows <tomburrows13@icloud.com>
2025-05-16 13:00:17 +0100

    Support additional_categories in heat-recipes.lua

--- prototypes/heat-recipes.lua
+++ prototypes/heat-recipes.lua
@@ -1,14 +1,24 @@
 local heat_recipes = {}
 
+function recipe_has_smelting_category(recipe)
+  return recipe.category == "smelting"
+    or recipe.category == "ll-fluid-smelting"
+    or recipe.category == "ll-electric-smelting"
+    or recipe.additional_categories and x_util.contains(recipe.additional_categories, "smelting")
+    or recipe.additional_categories and x_util.contains(recipe.additional_categories, "ll-fluid-smelting")
+    or recipe.additional_categories and x_util.contains(recipe.additional_categories, "ll-electric-smelting")
+end
+
 -- Add heat to arc smelting recipes and make alternate recipes with heat for other smelting recipes
 for name, recipe in pairs(data.raw.recipe) do
   if recipe.category == "ll-arc-smelting" and name:sub(-5, -1) ~= "-heat" then
     table.insert(recipe.results, {type = "fluid", name = "ll-heat", amount = recipe.energy_required, fluidbox_index = 2})
     table.insert(heat_recipes, name)
-  elseif recipe.category == "smelting" or recipe.category == "ll-fluid-smelting" or recipe.category == "ll-electric-smelting" and recipe.name:sub(-5, -1) ~= "-heat" then
+  elseif recipe_has_smelting_category(recipe) and recipe.name:sub(-5, -1) ~= "-heat" then
     local heat_recipe = table.deepcopy(recipe)
     heat_recipe.name = name .. "-heat"
     heat_recipe.category = "ll-arc-smelting"
+    heat_recipe.additional_categories = nil
     heat_recipe.localised_name = recipe.localised_name or {"?", {"item-name." .. name}, {"recipe-name." .. name}}
     heat_recipe.localised_description = recipe.localised_description or {"?", {"item-description." .. name}, {"?", {"recipe-description." .. name}, ""}}
 
